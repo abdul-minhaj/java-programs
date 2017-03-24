2f9e42914fa182b879c27aa0d23c3f355a5a0617
abdul minhaj <abdul_minhaj@ymail.com>
2017-03-25 01:00:08 +0530

    Update Way2SMS.java

--- Selenium Programs/Way2SMS.java
+++ Selenium Programs/Way2SMS.java
@@ -14,24 +14,29 @@ public class Way2SMS {
 		WebDriver driver = new ChromeDriver ();
 		driver.get("http://way-2-sms.in/way2sms-login/");
 		driver.findElement(By.className("alignnone")).click();
-		//Thread.sleep(5000);
-		driver.findElement(By.id("username")).sendKeys("9610275514");
-		driver.findElement(By.id("password")).sendKeys("minhaj786");
-		driver.findElement(By.id("loginBTN")).click();
-		driver.findElement(By.xpath("//*[@class = 'but reds1 fl skip']")).click();
-		driver.findElement(By.xpath("//*[@value='Send Free SMS']")).click();
-		driver.findElement(By.linkText("Send SMS")).click();
-		driver.findElement(By.id("mobile")).sendKeys("9610275514");
-		driver.findElement(By.id("message")).sendKeys("good night");
-		driver.findElement(By.id("send")).sendKeys("good night");
-		driver.findElement(By.className("lout")).click();
-		
-		
-		
-		/* INPUT[@type='button'])[2]
-		
 		Thread.sleep(5000);
-		driver.close();*/
+		driver.findElement(By.id("username")).sendKeys("XXXXXXXX");
+		driver.findElement(By.id("password")).sendKeys("XXXXXXXX");
+		Thread.sleep(5000);
+		driver.findElement(By.id("loginBTN")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@class = 'but reds1 fl skip']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@value='Send Free SMS']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@id='sendSMS']/child::*")).click();
+		Thread.sleep(5000);
+		driver.switchTo().frame("frame");// "FRAME" is frame name (syntax: driver.switchTo().frame("FRAME NAME");)
+		driver.findElement(By.id("mobile")).sendKeys("XXXXXXXXXX");
+		driver.findElement(By.id("message")).sendKeys("no responce ????");
+		driver.findElement(By.id("Send")).click();
+		driver.findElement(By.xpath("//*[@class='mess']/descendant::*[5]"));
+		String x = driver.findElement(By.xpath("//*[@class='mess']/descendant::*[5]")).getText();
+		System.out.println(x);
+		driver.switchTo().defaultContent();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@class='out louti']")).click();
+		driver.close();
 		
 
 	}
